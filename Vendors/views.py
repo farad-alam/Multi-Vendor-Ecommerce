e9cc262633bf29483dd36de3a022d6db067e46c2
farad-alam <farad.alam1997@gmail.com>
2023-11-12 10:05:08 +0600

    giving permission when vendor are created and display store name in the home page

--- Vendors/views.py
+++ Vendors/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
 from django.contrib import messages
+from django.contrib.auth.models import Group
+
 # Create your views here.
 
+
 def vendor_registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
             preview_form = form.save(commit=False)
             preview_form.user_role = 3
+            # preview_form.is_staff = False
             preview_form.save()
-            messages.info(request,'Your Vendor Account Created Successfully!!!!')
-            return redirect('/vendor-dashboard/login/')
+
+            # get the targeted group where the user can acceess
+            vendor_group = Group.objects.get(name="Vendors")
+
+            # add user to permisssion group
+            preview_form.groups.add(vendor_group)
+
+            messages.info(request, "Your Vendor Account Created Successfully!!!!")
+            return redirect("/vendor-dashboard/login/")
     else:
         form = RegistrationForm()
-    context = {
-        'form': form
-    }
-    return render(request,'accounts/vendor/vendor_registration.html',context)
+    context = {"form": form}
+    return render(request, "accounts/vendor/vendor_registration.html", context)
